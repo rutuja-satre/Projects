2a319b259ee63b40ca6f8d80b729ee926e9ef750
Rutuja Satre <rutusatre355@gmail.com>
2026-02-15 02:00:13 +0530

    Add files via upload
    The EduTrack Portal is a full - stack Java - based web+client application designed to manage and streamline academic activities.The portal provides CRUD operations for managing batches along with detailed student records in each batch. It leverages Spring Boot for building a RESTful backed, MongoDB for scalable data storage, and a Java client application for user interaction. The system simplifies batch creation,student management and record updates, making it a centralized classroom management solution.

---
+++ Edu-track/MarvellousFullStack/src/main/java/com/marvellous/MarvellousFullStack/Entity/BatchEntry.java
@@ -0,0 +1,16 @@
+package com.marvellous.MarvellousFullStack.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "BatchDetails")
+@Getter
+@Setter
+public class BatchEntry
+{
+    private ObjectId id;
+    private String name;
+    private int fees;
+}
